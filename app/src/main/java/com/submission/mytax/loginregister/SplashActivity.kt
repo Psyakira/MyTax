5b4b5cc8bc2346c7e04b319cd155eb7424418001
Fauziyyah Layla <fauzi@fauziyyah_layla.com>
2024-06-02 11:29:31 +0700

    splash and welcome activity

---
+++ app/src/main/java/com/submission/mytax/loginregister/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.submission.mytax.loginregister
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.submission.mytax.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 2000L)
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
