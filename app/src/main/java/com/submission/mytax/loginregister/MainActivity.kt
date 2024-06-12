15549d0f734bbdef26bf5483a778119f1ef6076f
muhfarian <12150114744@students.uin-suska.ac.id>
2024-06-12 13:35:50 +0700

    perbaikan dari error yang di push splash and welcome activity, kemudian menabmhankan tombol login menggunakan google

--- app/src/main/java/com/submission/mytax/loginregister/MainActivity.kt
+++ app/src/main/java/com/submission/mytax/loginregister/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.submission.mytax.loginregister
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +13,17 @@ import com.submission.mytax.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToWelcomeActivity()
+        }, 2000L)
+    }
+
+    private fun goToWelcomeActivity() {
+        Intent(this, WelcomeActivity::class.java).also {
+            startActivity(it)
+            finish()
         }
     }
 }
