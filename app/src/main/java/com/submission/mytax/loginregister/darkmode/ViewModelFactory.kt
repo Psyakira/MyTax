884a64fa935ea396a4abc8d44d351944eafa41e5
fauzi <lailanurul@gmail.com>
2024-06-13 14:29:59 +0700

    Penerapan fungsi tema gelap

---
+++ app/src/main/java/com/submission/mytax/loginregister/darkmode/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.submission.mytax.loginregister.darkmode
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.submission.mytax.loginregister.MainViewModel
+
+class ViewModelFactory (private val pref: SettingPreferences) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DarkModeViewModel::class.java)) {
+            return DarkModeViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
